f6bf3d95e034e5e3e55a1726cd82370243fd7ec1
Anna <av.wilde04+github@gmail.com>
2021-05-03 16:03:13 +0100

    Display the title of each todo item

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,3 +1,4 @@
+from todo_app.data import session_items
 from flask import Flask
 from flask import render_template
 
@@ -9,7 +10,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', items=session_items.get_items())
 
 
 if __name__ == '__main__':
