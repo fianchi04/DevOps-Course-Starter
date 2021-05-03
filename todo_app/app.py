94c1f1af4276656633dd6ec5b9023f008740a17c
Anna <av.wilde04+github@gmail.com>
2021-05-03 16:20:02 +0100

    add a new todo and see item appear on list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
 from todo_app.data import session_items
 from flask import Flask
 from flask import render_template
+from flask import request
 
 from todo_app.flask_config import Config
 
@@ -8,9 +9,15 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html', items=session_items.get_items())
+    if request.method == 'POST':
+        session_items.add_item(request.form.get('title'))
+        return render_template('index.html', items=session_items.get_items()) 
+    if request.method == 'GET':
+        return render_template('index.html', items=session_items.get_items())
+
+
 
 
 if __name__ == '__main__':
