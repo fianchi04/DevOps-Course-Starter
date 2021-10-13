3fc439ff30ffe269e228a23f475ee4b01aaef443
Anna Wilde <av.wilde04+github@gmail.com>
2021-10-13 19:47:07 +0100

    separated todo and done items on index page

--- todo_app/app.py
+++ todo_app/app.py
@@ -8,15 +8,17 @@ from todo_app.flask_config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
-@app.route('/', methods = ['GET'])
+@app.route('/')
 def index():
-    return render_template('index.html', items=todo_items.get_todos())
+    return render_template('index.html', todos=todo_items.get_todos(), dones=todo_items.get_dones())
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def post():
     todo_items.create_todo(request.form.get('title'))
-    return render_template('index.html', items=todo_items.get_todos())
+    return render_template('index.html', todos=todo_items.get_todos(), dones=todo_items.get_dones())
+
+#def update_card_list():
+
 
 if __name__ == '__main__':
     app.run()
