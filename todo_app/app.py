f79dc5a2278d4691250a17dc8900f5c99c5b36ca
Anna Wilde <av.wilde04+github@gmail.com>
2021-10-13 18:26:20 +0100

    refactored get and post to index into two different methods

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,23 +3,20 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods = ['GET'])
 def index():
-    if request.method == 'POST':
-        todo_items.create_todo(request.form.get('title'))
-        return render_template('index.html', items=todo_items.get_todos())
-        ##todo: separate routes for adding todo https://github.com/fianchi04/DevOps-Course-Starter/pull/1#discussion_r628209452
+    return render_template('index.html', items=todo_items.get_todos())
 
-    if request.method == 'GET':
-        return render_template('index.html', todo_items.get_todos())
-
+@app.route('/', methods = ['POST'])
+def post():
+    todo_items.create_todo(request.form.get('title'))
+    return render_template('index.html', items=todo_items.get_todos())
 
 if __name__ == '__main__':
     app.run()
