18a29e69a9dd4cfb05a947e3da80cc1b361034a8
Anna Wilde <av.wilde04+github@gmail.com>
2021-10-13 16:05:25 +0100

    Trello key and token stored and called from .env file, and starter implementation for creating a card on trello todo board

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,8 @@
-from todo_app.data import session_items
+from todo_app.data import session_items, todo_items
 from flask import Flask
 from flask import render_template
 from flask import request
+
 
 from todo_app.flask_config import Config
 
@@ -12,12 +13,13 @@ app.config.from_object(Config)
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        todo_items.create_todo()
         session_items.add_item(request.form.get('title'))
         return render_template('index.html', items=session_items.get_items()) 
+        ##todo: separate routes for adding todo https://github.com/fianchi04/DevOps-Course-Starter/pull/1#discussion_r628209452
+
     if request.method == 'GET':
         return render_template('index.html', items=session_items.get_items())
-
-
 
 
 if __name__ == '__main__':
